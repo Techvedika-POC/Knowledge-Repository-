dae390821b47a70ce866d725b4d1ff9deac868a0
prameela.sangati <prameela.sangati@DESKTOP-O9S88UI>
2025-09-28 21:20:52 +0530

    Registrationa and Login Backend

--- knowledge-frontend/tailwind.config.js
+++ knowledge-frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // <--- scans all your React components
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: { extend: {} },
   plugins: [],
 };
