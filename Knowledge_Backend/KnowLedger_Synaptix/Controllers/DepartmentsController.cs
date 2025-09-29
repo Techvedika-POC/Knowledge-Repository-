340585b53fbd7a9e9ec150f06e18ad878ff7ca25
prameela.sangati <prameela.sangati@DESKTOP-O9S88UI>
2025-09-29 12:37:31 +0530

    Registration and Login full functionality

---
+++ Knowledge_Backend/KnowLedger_Synaptix/Controllers/DepartmentsController.cs
@@ -0,0 +1,25 @@
+﻿using KnowLedger_Synaptix.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace KnowLedger_Synaptix.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IDepartmentService _departmentService;
+
+        public DepartmentsController(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDepartments()
+        {
+            var departments = await _departmentService.GetDepartmentsAsync();
+            return Ok(departments);
+        }
+    }
+}
--- Knowledge_Backend/KnowLedger_Synaptix/Models/Role.cs
+++ Knowledge_Backend/KnowLedger_Synaptix/Models/Role.cs
@@ -2,7 +2,6 @@
 #nullable disable
 using System;
 using System.Collections.Generic;
-using KnowLedger_Synaptix.Enums;
 
 namespace KnowLedger_Synaptix.Models;
 
@@ -10,7 +9,8 @@ public partial class Role
 {
     public Guid RoleId { get; set; }
 
-    public RoleName RoleName { get; set; }
+    public string RoleName { get; set; }
+
     public string Description { get; set; }
 
     public DateTime? CreatedOn { get; set; }
