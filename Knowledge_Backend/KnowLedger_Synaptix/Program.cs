1b238c6a273e23792588c207a8586e951763ae84
dinesh-kandula <144089646+dinesh-kandula@users.noreply.github.com>
2025-10-10 22:33:15 +0530

    Update Program.cs

--- Knowledge_Backend/KnowLedger_Synaptix/Program.cs
+++ Knowledge_Backend/KnowLedger_Synaptix/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
         policy => policy
-            .WithOrigins("http://localhost:3000") 
+            .WithOrigins("http://localhost:3000", "https://knowledge-frontend-n567.onrender.com/") 
             .AllowAnyHeader()
             .AllowAnyMethod()
             .AllowCredentials());
