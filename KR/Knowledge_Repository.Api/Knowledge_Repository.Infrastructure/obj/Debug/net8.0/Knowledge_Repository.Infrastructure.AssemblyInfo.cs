a41c7869c39238c0823918ce3ff2846e8976ef3d
prameela.sangati <prameela.sangati@DESKTOP-O9S88UI>
2025-11-12 14:40:45 +0530

    Updated

--- KR/Knowledge_Repository.Api/Knowledge_Repository.Infrastructure/obj/Debug/net8.0/Knowledge_Repository.Infrastructure.AssemblyInfo.cs
+++ KR/Knowledge_Repository.Api/Knowledge_Repository.Infrastructure/obj/Debug/net8.0/Knowledge_Repository.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Knowledge_Repository.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfe57fcfd36b6c95d2422f95e04ab2270d11c620")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfef318f1948125a12a108f1bc386aca4ffa77dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Knowledge_Repository.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Knowledge_Repository.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
